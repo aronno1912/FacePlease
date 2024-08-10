d24efb847f61e97bd1b54e056c1a50c9fef73916
aronno1912 <debjanyaronno@gmail.com>
2024-08-10 13:07:14 +0600

    installed packages

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+import numpy as np
+import csv
+import os
+from datetime import datetime
+import cv2
+import dlib
+import face_recognition
+
+print("hello world")
+print("dlib version:", dlib.__version__)
