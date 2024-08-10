7b059311b1310a9d87a4d838493728ebfbc583a3
aronno1912 <debjanyaronno@gmail.com>
2024-08-10 13:59:25 +0600

    version 1 works fine!!

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import dlib
 import face_recognition
 
 
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)  ### 0 because taking input from default camera
  
 jobs_image = face_recognition.load_image_file("photos/jobs.jpg")
 jobs_encoding = face_recognition.face_encodings(jobs_image)[0]
@@ -55,60 +55,115 @@ students = known_faces_names.copy()
 
 face_locations = []
 face_encodings = []
-face_names = []
-s=True
+
 
 now = datetime.now()
 current_date = now.strftime("%Y-%m-%d")
 
-f = open(current_date+'.csv','w+',newline = '')
+f = open(f"{current_date}.csv" , "w+" , newline="")
 lnwriter = csv.writer(f)
 
-
-
- 
 while True:
-    _,frame = video_capture.read()
-    small_frame = cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
-    rgb_small_frame = small_frame[:,:,::-1]
-    if s:
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame,face_locations)
-        face_names = []
-        for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(known_face_encoding,face_encoding)
-            name=""
-            face_distance = face_recognition.face_distance(known_face_encoding,face_encoding)
-            best_match_index = np.argmin(face_distance)
-            if matches[best_match_index]:
-                name = known_faces_names[best_match_index]
- 
-            face_names.append(name)
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame ,(0,0) , fx=0.25 , fy=0.25)
+    rgb_small_frame = cv2.cvtColor(small_frame , cv2.COLOR_BGR2RGB) 
+    
+    # Recognise faces
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame , face_locations)
+    
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_face_encodings,face_encoding)
+        face_distance = face_recognition.face_distance(known_face_encodings,face_encoding)
+        best_match_index = np.argmin(face_distance)
+        
+        if(matches[best_match_index]):
+            name = known_faces_names[best_match_index]
+            
+            # Add the text if a person is present
             if name in known_faces_names:
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 bottomLeftCornerOfText = (10,100)
-                fontScale              = 1.5
-                fontColor              = (255,0,0)
-                thickness              = 3
-                lineType               = 2
- 
-                cv2.putText(frame,name+' Present', 
-                    bottomLeftCornerOfText, 
-                    font, 
-                    fontScale,
-                    fontColor,
-                    thickness,
-                    lineType)
- 
+                fontScale = 1.5
+                fontColor = (255,0,0)
+                thickness = 3
+                lineType = 2
+                cv2.putText(frame, name + " Present", bottomLeftCornerOfText, font, fontScale, fontColor, thickness, lineType)
+                
                 if name in students:
                     students.remove(name)
-                    print(students)
-                    current_time = now.strftime("%H-%M-%S")
+                    current_time = now.strftime("%H:%M:%S")
                     lnwriter.writerow([name,current_time])
-    cv2.imshow("attendence system",frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    
+    cv2.imshow("Camera" , frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
- 
+    
 video_capture.release()
 cv2.destroyAllWindows()
 f.close()
+
+
+
+#face_names = []
+# s=True
+
+# now = datetime.now()
+# current_date = now.strftime("%Y-%m-%d")
+
+# f = open(current_date+'.csv','w+',newline = '')
+# lnwriter = csv.writer(f)
+
+
+
+ 
+# while True:
+#     _,frame = video_capture.read()
+#     small_frame = cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
+#     rgb_small_frame = small_frame[:,:,::-1]
+#     if s:
+#         face_locations = face_recognition.face_locations(rgb_small_frame)
+#         face_encodings = face_recognition.face_encodings(rgb_small_frame,face_locations)
+#         face_names = []
+#         for face_encoding in face_encodings:
+#             matches = face_recognition.compare_faces(known_face_encoding,face_encoding)
+#             name=""
+#             face_distance = face_recognition.face_distance(known_face_encoding,face_encoding)
+#             best_match_index = np.argmin(face_distance)
+#             if matches[best_match_index]:
+#                 name = known_faces_names[best_match_index]
+#                 print("the face is matched with ",name)
+ 
+#             face_names.append(name)
+#             if name in known_faces_names:
+#                 font = cv2.FONT_HERSHEY_SIMPLEX
+#                 bottomLeftCornerOfText = (10,100)
+#                 fontScale              = 1.5
+#                 fontColor              = (255,0,0)
+#                 thickness              = 3
+#                 lineType               = 2
+ 
+#                 cv2.putText(frame,name+' Present', 
+#                     bottomLeftCornerOfText, 
+#                     font, 
+#                     fontScale,
+#                     fontColor,
+#                     thickness,
+#                     lineType)
+ 
+#                 if name in students:
+#                     students.remove(name)
+#                     print(students)
+#                     current_time = now.strftime("%H-%M-%S")
+#                     lnwriter.writerow([name,current_time])
+#     cv2.imshow("attendence system",frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+ 
+# video_capture.release()
+# cv2.destroyAllWindows()
+# f.close()
+
+
+
+
